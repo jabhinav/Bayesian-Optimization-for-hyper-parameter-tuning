e0f3cb7e628bbfd101c98c77c5af6c2f600f4ea2
Abhinav Jain <jain.abhinav05@gmail.com>
2016-11-25 01:17:47 +0530

    Update softmaxPredict.m

--- softmaxPredict.m
+++ softmaxPredict.m
@@ -4,16 +4,13 @@ function [pred] = softmaxPredict(softmaxModel, data)
 % data - the N x M input matrix, where each column data(:, i) corresponds to
 %        a single test set
 %
-% Your code should produce the prediction matrix 
-% pred, where pred(i) is argmax_c P(y(c) | x(i)).
+
  
 % Unroll the parameters from theta
 theta = softmaxModel.optTheta;  % this provides a numClasses x inputSize matrix
 pred = zeros(1, size(data, 2));
 
-%% ---------- YOUR CODE HERE --------------------------------------
-%  Instructions: Compute pred using theta assuming that the labels start 
-%                from 1.
+
 M = theta*data;
 M = bsxfun(@minus, M, max(M, [], 1));
 M = exp(M);
